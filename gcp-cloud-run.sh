ac5cf658ff967f5dabf2b3f34a0f2930e1ff8719
andrewzinkyaw <andrewzinkyaw@gmail.com>
2025-10-16 17:52:13 +0630

    Update gcp-cloud-run.sh

--- gcp-cloud-run.sh
+++ gcp-cloud-run.sh
@@ -530,11 +530,11 @@ main() {
 
     DOMAIN=$(echo $SERVICE_URL | sed 's|https://||')
 
-    # 🕒 Start time (MMT)
-START_TIME=$(TZ='Asia/Yangon' date +"%Y-%m-%d %H:%M:%S")
+    # 🕒 Start time (MMT, 12-hour format)
+START_TIME=$(TZ='Asia/Yangon' date +"%Y-%m-%d %I:%M:%S %p")
 
-# ⏰ End time = 5 hours from now (MMT)
-END_TIME=$(TZ='Asia/Yangon' date -d "+5 hours" +"%Y-%m-%d %H:%M:%S")
+# ⏰ End time = 5 hours from now (MMT, 12-hour format)
+END_TIME=$(TZ='Asia/Yangon' date -d "+5 hours" +"%Y-%m-%d %I:%M:%S %p")
 
     # VLESS link
     VLESS_LINK="vless://${UUID}@${HOST_DOMAIN}:443?path=%2Ftg-%40trenzych&security=tls&alpn=h3%2Ch2%2Chttp%2F1.1&encryption=none&host=${DOMAIN}&fp=randomized&type=ws&sni=${DOMAIN}#${SERVICE_NAME}"
